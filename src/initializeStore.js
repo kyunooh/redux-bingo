9db1db01d2fddd883dc9d27211b3130ba24b5c79
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-06-16 19:43:36 +0900

    Remove redux devtools for deploy

--- src/initializeStore.js
+++ src/initializeStore.js
@@ -3,8 +3,7 @@ import reducers from "./reducers";
 
 const initializeStore = () => {
   const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducers
   );
   return store;
 };
